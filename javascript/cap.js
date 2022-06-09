54c8be8775a9a3cad376ebbc87eadcfcfe7b4556
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-06-09 11:46:03 -0300

    Adição do global css e refatoração

---
+++ javascript/cap.js
@@ -0,0 +1,16 @@
+function toTheTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+let butao = document.querySelector('#return')
+
+function dinamicButton() {
+    if (window.scrollY == 0) {
+        butao.classList.add('invisible')
+    } else {
+        butao.classList.remove('invisible')
+    }
+}
+window.addEventListener('scroll', dinamicButton)
